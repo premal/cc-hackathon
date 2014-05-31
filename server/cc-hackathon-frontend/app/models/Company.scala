1bfee8b6d2c523cf8c34332e7d4d5bd0c9ce9e55
yoocos <david@yoocos.com>
2014-05-31 17:46:45 -0400

    no acquisitions data used

--- server/cc-hackathon-frontend/app/models/Company.scala
+++ server/cc-hackathon-frontend/app/models/Company.scala
@@ -17,19 +17,16 @@ object CompanyRepository {
 
   def init{
     val companiesData = Source.fromFile(new File("../../data/crunchbase_companies.csv"))("UTF-8").getLines().map(_.split(";").map(_.trim))
-    val acquisitionsData = Source.fromFile(new File("../../data/crunchbase_acquisitions.csv"))("UTF-8").getLines().map(_.split(";").map(_.trim))
     val roundsData = Source.fromFile(new File("../../data/crunchbase_rounds.csv"))("UTF-8").getLines().map(_.split(";").map(_.trim))
 
     val result = (for(
       companyHeader <- companiesData.take(1);
-      acquisitionHeader <- acquisitionsData.take(1);
       roundHeader <- roundsData.take(1)
     ) yield {
       val companies = companiesData.map(toEntry(companyHeader,_)).map(m => m("name") -> m).toMap
-      val acquisitions = acquisitionsData.map(toEntry(acquisitionHeader,_)).map(m => m.get("company_name").map(_ -> m)).filter(_.isDefined).map(_.get).toMap
       val rounds = roundsData.map(toEntry(roundHeader,_)).map(m => m("company_name") -> m).toMap
 
-      companies.keys.map(createCompanyElement(companies,acquisitions,rounds,_))
+      companies.keys.map(createCompanyElement(companies,rounds,_))
     })
     companies = result.next().toSeq
   }
@@ -38,9 +35,8 @@ object CompanyRepository {
 
   private def cleanDomain(url:String) = Option((new URI(url)).getHost).map(_.replace("www.","")).getOrElse("")
 
-  private def createCompanyElement(companies:Map[String,Map[String,String]],acquisitions:Map[String,Map[String,String]],rounds:Map[String,Map[String,String]],company:String): Seq[String] = {
+  private def createCompanyElement(companies:Map[String,Map[String,String]],rounds:Map[String,Map[String,String]],company:String): Seq[String] = {
     val companyData = companies.get(company).getOrElse(Map())
-    val acquisitionsData = acquisitions.get(company).getOrElse(Map())
     val roundsData = rounds.get(company).getOrElse(Map())
     Seq(
       company,
